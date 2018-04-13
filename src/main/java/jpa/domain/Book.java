567c674fada2e058d9e21c2e13f36286f2014f87
Lin Jerry <jerrylin.codemonkey@gmail.com>
2018-04-13 22:44:46 +0800

    format

--- src/main/java/jpa/domain/Book.java
+++ src/main/java/jpa/domain/Book.java
@@ -9,101 +9,102 @@ import javax.persistence.*;
 @Table(
         name = "Book",
         indexes = {
-                @Index(name = "idx_book_isbn13",columnList = "isbn13",unique = true),
-                @Index(name = "idx_book_isbn10",columnList = "isbn10",unique = true)
+                @Index(name = "idx_book_isbn13", columnList = "isbn13", unique = true),
+                @Index(name = "idx_book_isbn10", columnList = "isbn10", unique = true)
         }
 )
 public class Book {
 
-    @Id
-    @SequenceGenerator(name = "book_generator", sequenceName = "book_sequence", initialValue = 100)
-    @GeneratedValue(generator = "book_generator")
-    private Long id;
+  @Id
+  @SequenceGenerator(name = "book_generator", sequenceName = "book_sequence", initialValue = 100)
+  @GeneratedValue(generator = "book_generator")
+  private Long id;
 
-    @Column(length = 128, nullable = false)
-    private String title;
+  @Column(length = 128, nullable = false)
+  private String title;
 
-    protected Book(){}
+  protected Book() {
+  }
 
-    public Book( String title ){
-        this.title = title;
-    }
+  public Book(String title) {
+    this.title = title;
+  }
 
-    @Column(length = 13)
-    private String isbn13;
+  @Column(length = 13)
+  private String isbn13;
 
-    @Column(length = 10)
-    private String isbn10;
+  @Column(length = 10)
+  private String isbn10;
 
-    private int pages;
+  private int pages;
 
-    private String author;
+  private String author;
 
-    private String publisher;
+  private String publisher;
 
-    private long publishTime;
+  private long publishTime;
 
-    public Long getId() {
-        return id;
-    }
+  public Long getId() {
+    return id;
+  }
 
-    public void setId(Long id) {
-        this.id = id;
-    }
+  public void setId(Long id) {
+    this.id = id;
+  }
 
-    public String getTitle() {
-        return title;
-    }
+  public String getTitle() {
+    return title;
+  }
 
-    public void setTitle(String title) {
-        this.title = title;
-    }
+  public void setTitle(String title) {
+    this.title = title;
+  }
 
-    public String getIsbn13() {
-        return isbn13;
-    }
+  public String getIsbn13() {
+    return isbn13;
+  }
 
-    public void setIsbn13(String isbn13) {
-        this.isbn13 = isbn13;
-    }
+  public void setIsbn13(String isbn13) {
+    this.isbn13 = isbn13;
+  }
 
-    public String getIsbn10() {
-        return isbn10;
-    }
+  public String getIsbn10() {
+    return isbn10;
+  }
 
-    public void setIsbn10(String isbn10) {
-        this.isbn10 = isbn10;
-    }
+  public void setIsbn10(String isbn10) {
+    this.isbn10 = isbn10;
+  }
 
-    public int getPages() {
-        return pages;
-    }
+  public int getPages() {
+    return pages;
+  }
 
-    public void setPages(int pages) {
-        this.pages = pages;
-    }
+  public void setPages(int pages) {
+    this.pages = pages;
+  }
 
-    public String getAuthor() {
-        return author;
-    }
+  public String getAuthor() {
+    return author;
+  }
 
-    public void setAuthor(String author) {
-        this.author = author;
-    }
+  public void setAuthor(String author) {
+    this.author = author;
+  }
 
-    public String getPublisher() {
-        return publisher;
-    }
+  public String getPublisher() {
+    return publisher;
+  }
 
-    public void setPublisher(String publisher) {
-        this.publisher = publisher;
-    }
+  public void setPublisher(String publisher) {
+    this.publisher = publisher;
+  }
 
-    public long getPublishTime() {
-        return publishTime;
-    }
+  public long getPublishTime() {
+    return publishTime;
+  }
 
-    public void setPublishTime(long publishTime) {
-        this.publishTime = publishTime;
-    }
+  public void setPublishTime(long publishTime) {
+    this.publishTime = publishTime;
+  }
 }
