ae0f76bff2fc32a052e387ad466e7eccd07b71f0
Lin Jerry <jerrylin.codemonkey@gmail.com>
2018-04-23 17:11:53 +0800

    format

--- src/main/java/io/swagger/api/gen/api/ApiOriginFilter.java
+++ src/main/java/io/swagger/api/gen/api/ApiOriginFilter.java
@@ -9,24 +9,27 @@ import javax.servlet.*;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-@javax.annotation.Generated(value = "io.swagger.codegen.languages.java.JavaJerseyServerCodegen", date = "2018-04-17T14:21:51.738+08:00[Asia/Taipei]")
+@javax.annotation.Generated(
+  value = "io.swagger.codegen.languages.java.JavaJerseyServerCodegen",
+  date = "2018-04-17T14:21:51.738+08:00[Asia/Taipei]"
+)
 public class ApiOriginFilter implements Filter {
 
-    private final transient Log logger = LogFactory.getLog(this.getClass());
+  private final transient Log logger = LogFactory.getLog(this.getClass());
 
-    public void doFilter(ServletRequest request, ServletResponse response,
-            FilterChain chain) throws IOException, ServletException {
-        HttpServletResponse res = (HttpServletResponse) response;
-        res.addHeader("Access-Control-Allow-Origin", "*");
-        res.addHeader("Access-Control-Allow-Methods", "GET, POST, DELETE, PUT");
-        res.addHeader("Access-Control-Allow-Headers", "Content-Type");
-        long time = System.currentTimeMillis();
-        chain.doFilter(request, response);
-        time = System.currentTimeMillis() - time;
-        logger.trace(((HttpServletRequest) request).getRequestURI() +" : " + time + " ms ");
-    }
+  public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain)
+      throws IOException, ServletException {
+    HttpServletResponse res = (HttpServletResponse) response;
+    res.addHeader("Access-Control-Allow-Origin", "*");
+    res.addHeader("Access-Control-Allow-Methods", "GET, POST, DELETE, PUT");
+    res.addHeader("Access-Control-Allow-Headers", "Content-Type");
+    long time = System.currentTimeMillis();
+    chain.doFilter(request, response);
+    time = System.currentTimeMillis() - time;
+    logger.trace(((HttpServletRequest) request).getRequestURI() + " : " + time + " ms ");
+  }
 
-    public void destroy() {}
+  public void destroy() {}
 
-    public void init(FilterConfig filterConfig) throws ServletException {}
+  public void init(FilterConfig filterConfig) throws ServletException {}
 }
