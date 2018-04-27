875f71fb4514f41c96439f390e9597f748b75aac
Lin Jerry <jerrylin.codemonkey@gmail.com>
2018-04-27 12:12:09 +0800

    format

--- src/test/java/logic/parser/impl/BooksDotComBookParserTest.java
+++ src/test/java/logic/parser/impl/BooksDotComBookParserTest.java
@@ -4,7 +4,6 @@ import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import jpa.domain.Book;
 import logic.parser.Hendler.BookParserHandler;
-import logic.parser.IBookParser;
 import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 
@@ -21,24 +20,54 @@ public class BooksDotComBookParserTest {
   @DataProvider(name = "testBook")
   public Object[][] createData1() {
     return new Object[][] {
-            { "9789862419830", "Google模式：挑戰瘋狂變化世界的經營思維與工作邏輯", "How Google Works",
-            "9789862419830", 352, "Eric Schmidt, Jonathan Rosenberg", "天下雜誌", "2014/11/07",
-            "http://www.books.com.tw/products/0010656961"},
-
-            {"9789579609203", "第十年的情人節", "素敵な日本人",
-            "9789579609203", 288, "東野圭吾", "春天出版社", "2018/02/28",
-            "http://www.books.com.tw/products/0010780322"},
-
-            {"9789864002047", "在家自學的四個孩子", null,
-            "9789864002047", 224, "許惠珺", "道聲", "2018/04/10",
-            "http://www.books.com.tw/products/0010783929"}
+      {
+        "9789862419830",
+        "Google模式：挑戰瘋狂變化世界的經營思維與工作邏輯",
+        "How Google Works",
+        "9789862419830",
+        352,
+        "Eric Schmidt, Jonathan Rosenberg",
+        "天下雜誌",
+        "2014/11/07",
+        "http://www.books.com.tw/products/0010656961"
+      },
+      {
+        "9789579609203",
+        "第十年的情人節",
+        "素敵な日本人",
+        "9789579609203",
+        288,
+        "東野圭吾",
+        "春天出版社",
+        "2018/02/28",
+        "http://www.books.com.tw/products/0010780322"
+      },
+      {
+        "9789864002047",
+        "在家自學的四個孩子",
+        null,
+        "9789864002047",
+        224,
+        "許惠珺",
+        "道聲",
+        "2018/04/10",
+        "http://www.books.com.tw/products/0010783929"
+      }
     };
   }
 
   @Test(dataProvider = "testBook")
-  public void testGetBook(String key, String title, String originalTile,
-                          String ISBN13, int pages, String authors, String publisher, String publishedDate,
-                          String itemUrl ) throws ParseException {
+  public void testGetBook(
+      String key,
+      String title,
+      String originalTile,
+      String ISBN13,
+      int pages,
+      String authors,
+      String publisher,
+      String publishedDate,
+      String itemUrl)
+      throws ParseException {
 
     BookParserHandler iBookParser = new BooksDotComBookParser();
     Optional<Book> bookParsed = iBookParser.getBook(key);
@@ -64,6 +93,5 @@ public class BooksDotComBookParserTest {
     } catch (JsonProcessingException e) {
       e.printStackTrace();
     }
-
   }
 }
