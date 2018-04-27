459e3968e81528badab2a938d507aae5b2b314d3
Lin Jerry <jerrylin.codemonkey@gmail.com>
2018-04-27 12:13:15 +0800

    format

--- src/test/java/jpa/repositoryimpl/BookRepositoryImplTest.java
+++ src/test/java/jpa/repositoryimpl/BookRepositoryImplTest.java
@@ -11,11 +11,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.context.annotation.ComponentScan;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 import org.springframework.test.context.junit4.SpringRunner;
-import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 
@@ -23,49 +21,47 @@ import static org.assertj.core.api.Assertions.assertThat;
 import static org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase.Replace.NONE;
 
 @RunWith(SpringRunner.class)
-
 @SpringBootTest(classes = SampleJerseyApplication.class)
 @DataJpaTest
 @AutoConfigureTestDatabase(replace = NONE)
 public class BookRepositoryImplTest {
 
-    @Autowired
-    BookRepositoryImpl bookRepository;
+  @Autowired BookRepositoryImpl bookRepository;
 
-    Book _book = null;
+  Book _book = null;
 
-    @Before
-    public void prepareBook(){
-        _book = new Book("Test Book");
-        _book.setIsbn10("1111111111");
-        _book.setIsbn13("1111111111111");
-    }
+  @Before
+  public void prepareBook() {
+    _book = new Book("Test Book");
+    _book.setIsbn10("1111111111");
+    _book.setIsbn13("1111111111111");
+  }
 
-    @Test
-    public void findAll() {
-        assertThat(bookRepository.addBook(_book)).isTrue();
-        BookSearchCriteria criteria = new BookSearchCriteria();
-        Pageable pageable = PageRequest.of(0, 10);
-        List<Book> books = bookRepository.findAll(criteria, pageable);
-        assertThat(books.size() > 0).isTrue();
-        assertThat(books.size() <= 10).isTrue();
-    }
+  @Test
+  public void findAll() {
+    assertThat(bookRepository.addBook(_book)).isTrue();
+    BookSearchCriteria criteria = new BookSearchCriteria();
+    Pageable pageable = PageRequest.of(0, 10);
+    List<Book> books = bookRepository.findAll(criteria, pageable);
+    assertThat(books.size() > 0).isTrue();
+    assertThat(books.size() <= 10).isTrue();
+  }
 
-    @Ignore
-    public void addBook() {
-        //findAll()
-    }
+  @Ignore
+  public void addBook() {
+    // findAll()
+  }
 
-    @Test
-    public void updateBook() {
-        assertThat(bookRepository.addBook(_book)).isTrue();
-        _book.setTitle("Updated Book Title");
-        assertThat(bookRepository.updateBook(_book)).isTrue();
-    }
+  @Test
+  public void updateBook() {
+    assertThat(bookRepository.addBook(_book)).isTrue();
+    _book.setTitle("Updated Book Title");
+    assertThat(bookRepository.updateBook(_book)).isTrue();
+  }
 
-    @Test
-    public void deleteBook() {
-        assertThat(bookRepository.addBook(_book)).isTrue();
-        assertThat(bookRepository.deleteBook(_book)).isTrue();
-    }
+  @Test
+  public void deleteBook() {
+    assertThat(bookRepository.addBook(_book)).isTrue();
+    assertThat(bookRepository.deleteBook(_book)).isTrue();
+  }
 }
