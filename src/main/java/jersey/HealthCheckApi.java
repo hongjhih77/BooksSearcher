362931575b20af4b72137b104188555cc22e3db3
Lin Jerry <jerrylin.codemonkey@gmail.com>
2018-04-29 00:51:54 +0800

    health check

--- src/main/java/jersey/HealthCheckApi.java
+++ src/main/java/jersey/HealthCheckApi.java
@@ -1,18 +1,19 @@
-package io.swagger.api.gen.api;
+package jersey;
 
 import org.springframework.stereotype.Component;
 
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
+import javax.ws.rs.core.Context;
 import javax.ws.rs.core.Response;
+import javax.ws.rs.core.SecurityContext;
 
 @Component
 @Path("/healthcheck")
 public class HealthCheckApi {
   @GET
-  @Produces({"application/json"})
-  public Response booksIsbnGet() {
+  public Response booksIsbnGet(@Context SecurityContext securityContext) {
     return Response.ok().build();
   }
 }
