2a090edebfa25e08fe22a92370afb262ae30095b
Lin Jerry <jerrylin.codemonkey@gmail.com>
2018-04-30 22:55:54 +0800

    log with platform name

--- src/main/java/io/swagger/api/gen/api/ApiOriginFilter.java
+++ src/main/java/io/swagger/api/gen/api/ApiOriginFilter.java
@@ -1,16 +1,12 @@
 package io.swagger.api.gen.api;
 
-import org.apache.commons.logging.Log;
-import org.apache.commons.logging.LogFactory;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
-
-import java.io.IOException;
-import java.util.logging.Logger;
+import util.LogService;
 
 import javax.servlet.*;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
 
 @javax.annotation.Generated(
   value = "io.swagger.codegen.languages.java.JavaJerseyServerCodegen",
@@ -19,10 +15,10 @@ import javax.servlet.http.HttpServletResponse;
 @Component
 public class ApiOriginFilter implements Filter {
 
-  @Value("${PLATFORM}")
-  private String platform;
+//  @Value("${PLATFORM}")
+//  private String platform;
 
-  private final Log logger = LogFactory.getLog(platform);
+//  private final transient Log logger = LogFactory.getLog(platform);
 
   public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain)
       throws IOException, ServletException {
@@ -33,7 +29,7 @@ public class ApiOriginFilter implements Filter {
     long time = System.currentTimeMillis();
     chain.doFilter(request, response);
     time = System.currentTimeMillis() - time;
-    logger.info(((HttpServletRequest) request).getRequestURI() + " : " + time + " ms ");
+    LogService.info(((HttpServletRequest) request).getRequestURI() + " : " + time + " ms ");
   }
 
   public void destroy() {}
--- src/main/java/util/LogService.java
+++ src/main/java/util/LogService.java
@@ -4,10 +4,21 @@ import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
 
 import java.util.logging.Logger;
 
+@Component
 public class LogService {
+
+  public static String LOGGER_NAME;
+
+  @Value("${PLATFORM}")
+  public void setDatabase(String logname) {
+    LOGGER_NAME = logname;
+  }
+
   public static void errorSaveJson(Object object, Throwable throwable, Class<?> clazz){
     Log log = LogFactory.getLog(Logger.GLOBAL_LOGGER_NAME);
     try {
@@ -20,6 +31,11 @@ public class LogService {
   public static void error(Throwable throwable, Class<?> clazz){
     Log log = LogFactory.getLog(Logger.GLOBAL_LOGGER_NAME);
     log.error(throwable);
+  }
+
+  public static void info(String msg){
+    Log log = LogFactory.getLog(LOGGER_NAME);
+    log.info(msg);
   }
 
 }
