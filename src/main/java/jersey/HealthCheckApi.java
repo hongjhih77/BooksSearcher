093301d676ab433705e6e9508017e9638ef4c13e
Lin Jerry <jerrylin.codemonkey@gmail.com>
2018-05-01 18:14:27 +0800

    log with platform name

--- src/main/java/jersey/HealthCheckApi.java
+++ src/main/java/jersey/HealthCheckApi.java
@@ -8,12 +8,15 @@ import javax.ws.rs.Produces;
 import javax.ws.rs.core.Context;
 import javax.ws.rs.core.Response;
 import javax.ws.rs.core.SecurityContext;
+import java.util.logging.Logger;
 
 @Component
 @Path("/healthcheck")
 public class HealthCheckApi {
+  private static final String PLATFORM =
+          System.getenv().getOrDefault("PLATFORM", Logger.GLOBAL_LOGGER_NAME);
   @GET
   public Response booksIsbnGet(@Context SecurityContext securityContext) {
-    return Response.ok().build();
+    return Response.ok("PLATFORM = " + PLATFORM).build();
   }
 }
